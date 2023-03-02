6b2cf4bca3ede6b9b9f517d7c2bd2165bf02c7c8
hilmanfzn <fauzanhilman9@gmail.com>
2023-03-02 10:47:38 +0700

    commit-first

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,12 @@
 function toggleMenu() {
     const getMenu = document.querySelector('.menu-toggle');
     getMenu.classList.toggle("Active")
+    showMenu()
+    console.log(getMenu);
+}
+
+function showMenu() {
+    const getMenu = document.querySelector('.menu-show');
+    getMenu.classList.toggle("show")
     console.log(getMenu);
 }
